7c7b7f83785ecef4a61a05f32d498ef0496fe10b
Sn0wBash <102054270+Sn0wBash@users.noreply.github.com>
2022-03-22 18:38:22 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,5 @@
+CONSUMER_KEY = '<Replace Me>'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = '<Replace Me>'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '<Replace Me>'#keep the quotes, replace this with your access token
+ACCESS_SECRET = '<Replace Me>'#keep the quotes, replace this with your access token secret
+SCREEN_NAME = '<Replace Me>'#keep the quotes, replace this with your @screen_name (only used in the unfollow script)
